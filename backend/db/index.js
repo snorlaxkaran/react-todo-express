27bf27d893f28c5b55912e466cc559e41e90495f
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-03-14 12:36:28 +0530

    added todo schema

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,2 +1,19 @@
 const mongoose = require("mongoose");
-const mongoPasscode = "EAO5S5WilfL2fPgP";
+// .env
+mongoose.connect(
+  "mongodb+srv://snorlaxkaran:mJoL3ehicdVz2C1f@react-todo-nodejs-appli.yfplu.mongodb.net/"
+);
+
+mJoL3ehicdVz2C1f;
+
+const TodoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  completed: Boolean,
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+module.exports = {
+  Todo,
+};
