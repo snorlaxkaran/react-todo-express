2819436405c7dbea5f2b0b9ca14c29f397401048
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-03-14 11:20:14 +0530

    he

---
+++ backend/db/index.js
@@ -0,0 +1,2 @@
+const mongoose = require("mongoose");
+const mongoPasscode = "EAO5S5WilfL2fPgP";
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 const port = 3000;
 app.use(express.json);
@@ -7,17 +8,33 @@ app.use(express.json);
 // description: string
 
 app.post("/todo", (req, res) => {
-  //
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      message: "Your sent the wrong inputs",
+    });
+    return;
+  }
+
+  // Put it in mongoDB
 });
 
-app.get("/todos", (req, res) => {
-  res.json({
-    message: "",
-  });
-});
+app.get("/todos", (req, res) => {});
 
 app.put("/completed", (req, res) => {
-  //
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatePayload);
+
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      message: "You sent the wrong inputs",
+    });
+
+    return;
+  }
+
+  //   Put it in mongoDB
 });
 
 app.listen(port, () => {
