3cf0e7a93cf3e4a0521fdf819e4b1b97b5758bb7
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-03-14 13:27:32 +0530

    hi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { CreateTodo } from "./components/CreateTodo";
+import { Todos } from "./components/Todos";
 
 function App() {
   return (
     <div>
       <CreateTodo />
+      <Todos />
     </div>
   );
 }
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,9 @@
+export function Todos() {
+  return (
+    <div>
+      <h2>Go to GYM</h2>
+      <h2>Eat protein</h2>
+      <h2>Eat chicken</h2>
+    </div>
+  );
+}
