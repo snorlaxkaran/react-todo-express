5b2935ceb276e23659ea0f464d098465af7cc1b5
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-03-14 11:04:13 +0530

    ko

---
+++ README.md
@@ -0,0 +1,8 @@
+## Todo Application
+
+This project contains a simple Todo application
+It has the following features
+
+- Anyone can create a todo
+- Anyone can see there existing todo
+- Anyone can mark a todo as done
---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json);
+
+app.post("/todo", (req, res) => {
+  //
+});
+
+app.get("/todos", (req, res) => {
+  res.json({
+    message: "",
+  });
+});
+
+app.put("/completed", (req, res) => {
+  //
+});
+
+app.listen(port, () => {
+  console.log(`App is listening to port ${port}`);
+});
